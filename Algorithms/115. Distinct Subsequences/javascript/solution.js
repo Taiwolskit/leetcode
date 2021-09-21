aac4f4a0464787641df99dbb5eea6d31b948eaa2
Rukeith Tseng <rukeith.tseng@mile.cloud>
2021-09-21 17:54:04 +0800

    Update 115

--- Algorithms/115. Distinct Subsequences/javascript/solution.js
+++ Algorithms/115. Distinct Subsequences/javascript/solution.js
@@ -6,18 +6,18 @@
 var numDistinct = function (s, t) {
     const m = t.length,
         n = s.length;
-    const memo = new Array(m + 1).fill(0);
+    const dp = new Array(m + 1).fill(0);
 
-    memo[0] = 1;
+    dp[0] = 1;
 
     for (let i = 1; i < n + 1; i++) {
         let prev = 1;
         for (let j = 1; j < m + 1; j++) {
-            let temp = memo[j];
-            memo[j] += t[j - 1] === s[i - 1] ? prev : 0;
+            let temp = dp[j];
+            dp[j] += t[j - 1] === s[i - 1] ? prev : 0;
             prev = temp;
         }
     }
 
-    return memo[m];
+    return dp[m];
 };
