fdcb430c17b6f025d845a9997498363107f327c1
Edwin Jarvis <fenglan.crazydance@gmail.com>
2021-09-27 23:50:12 +0800

    Add 782, 1293

---
+++ Algorithms/1293. Shortest Path in a Grid with Obstacles Elimination/python/solution.memory.py
@@ -0,0 +1,36 @@
+class Solution:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        """
+        row, col = len(grid)-1, len(grid[0])-1
+        estimate_step + step, k, i, j
+        estimate_step = row+col-i-j
+        """
+        if not grid or not grid[0]:
+            return 0
+
+        row, col = len(grid), len(grid[0])
+        status_k = [[-1] * col for _ in range(row)]
+        if grid[0][0] == 1:
+            if k == 0:
+                return -1
+            status_k[0][0] = k-1
+        else:
+            status_k[0][0] = k
+
+        h = [(row+col-2, -status_k[0][0], 0, 0, 0)]
+        moves = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+
+        while h:
+            g, k, i, j, step = heapq.heappop(h)
+            if i == row-1 and j == col-1:
+                return step
+            step += 1
+            for di, dj in moves:
+                if 0 <= i+di < row and 0 <= j+dj < col:
+                    newk = k+grid[i+di][j+dj]
+                    if -newk > status_k[i+di][j+dj]:
+                        status_k[i+di][j+dj] = -newk
+                        heapq.heappush(
+                            h, (row+col-i-di-j-dj-2+step, newk, i+di, j+dj, step))
+
+        return -1
---
+++ Algorithms/1293. Shortest Path in a Grid with Obstacles Elimination/python/solution.runtime.py
@@ -0,0 +1,34 @@
+class Solution:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        m = len(grid) - 1
+        n = len(grid[0]) - 1
+        shortest = m + n
+        if k >= shortest:
+            return shortest
+
+        visited = dict()
+        stack1 = [(0, 0, k, -1)]
+        stack2 = list()
+        while stack1 or stack2:
+            active = stack1 if stack1 else stack2
+            i, j, obstacles, distance = active.pop()
+            if not (0 <= i < len(grid) and 0 <= j < len(grid[0])):
+                continue
+            if grid[i][j] == 1:
+                obstacles -= 1
+
+            if obstacles < 0:
+                continue
+            if (i, j) in visited and visited[(i, j)] >= obstacles:
+                continue
+
+            visited[(i, j)] = obstacles
+            distance += 1
+            if (i, j) == (m, n):
+                return distance
+            stack1.extend([(i+1, j, obstacles, distance),
+                          (i, j+1, obstacles, distance)])
+            stack2.extend([(i-1, j, obstacles, distance),
+                          (i, j-1, obstacles, distance)])
+
+        return -1
