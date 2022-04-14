72e3e7245041a3de6ba2e03c9923264c842ce291
Edwin Jarvis <fenglan.crazydance@gmail.com>
2022-04-14 09:11:39 +0800

    update 700
    Signed-off-by: Edwin Jarvis <fenglan.crazydance@gmail.com>

--- Algorithms/700. Search in a Binary Search Tree/kotlin/solution.recursion.kt
+++ Algorithms/700. Search in a Binary Search Tree/kotlin/solution.recursion.kt
@@ -1,10 +1,11 @@
 /**
- * Example: var ti = TreeNode(5) var v = ti.`val` Definition for a binary tree node. class
- * TreeNode(var `val`: Int) {
- * ```
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
  *     var left: TreeNode? = null
  *     var right: TreeNode? = null
- * ```
  * }
  */
 class Solution {
